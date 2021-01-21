d8815aad02955477f6dcab64165a201cf6784f5c
陈张萌 <2017013678@secoder.net>
2021-01-21 22:40:59 +0800

    [lab0]

--- os/src/main.rs
+++ os/src/main.rs
@@ -23,7 +23,6 @@
 mod console;
 mod panic;
 mod sbi;
-mod interrupt;
 
 // 汇编编写的程序入口，具体见该文件
 global_asm!(include_str!("entry.asm"));
@@ -31,13 +30,8 @@ global_asm!(include_str!("entry.asm"));
 /// Rust 的入口函数
 ///
 /// 在 `_start` 为我们进行了一系列准备之后，这是第一个被调用的 Rust 函数
+#[no_mangle]
 pub extern "C" fn rust_main() -> ! {
-    // 初始化各种模块
-    interrupt::init();
-
-    unsafe {
-        llvm_asm!("ebreak"::::"volatile");
-    };
-
-    unreachable!();
+    println!("Hello rCore-Tutorial!");
+    panic!("end of rust_main")
 }
