710ffb0d17d020d11ae0d1eda3f4e7e7e3c5722b
陈张萌 <2017013678@secoder.net>
2021-02-21 10:24:55 +0800

    [lab2] physical memory allocate

--- os/src/main.rs
+++ os/src/main.rs
@@ -33,17 +33,26 @@ global_asm!(include_str!("entry.asm"));
 /// 在 `_start` 为我们进行了一系列准备之后，这是第一个被调用的 Rust 函数
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
-    println!("Hello rCore-Tutorial!");
-
+    // 初始化各种模块
     interrupt::init();
+    memory::init();
 
-    unsafe{
-        llvm_asm!("ebreak"::::"volatile");
-    };
+    // 动态内存分配测试
+    use alloc::boxed::Box;
+    use alloc::vec::Vec;
+    let v = Box::new(5);
+    assert_eq!(*v, 5);
+    core::mem::drop(v);
 
-    loop{}
+    let mut vec = Vec::new();
+    for i in 0..10000 {
+        vec.push(i);
+    }
+    assert_eq!(vec.len(), 10000);
+    for (i, value) in vec.into_iter().enumerate() {
+        assert_eq!(value, i);
+    }
+    println!("heap test passed");
 
-    unreachable!();
-
-    // panic!("end of rust_main");
+    panic!()
 }
